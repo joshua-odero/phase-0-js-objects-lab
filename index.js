07f44a4223e2f810d26225e1e77aae81ab205d96
joshua-odero <misikojodero@gmail.com>
2025-11-24 18:59:36 +0300

    Modifying index.js with object functions

--- index.js
+++ index.js
@@ -1,6 +1,55 @@
-//Write your code here
+//Object declaration
+const attendee = {
 
+  attendeeId:"T001",
+  name:"Alice Smith",
+  event:"JavaScript Conference",
+  ticketType:"VIP",
+  ticketPrice:150.00
 
+}
+
+//Function to log the name of the attendee
+function logAttendeeName(attendee) {
+
+  console.log(attendee.name);
+  
+}
+
+//Function to log the price of the ticket
+function logTicketPrice(attendee) {
+
+  console.log(attendee.ticketPrice);
+
+}
+
+//Function to update the type of ticket
+function updateTicketType(attendee, newTicketType) {
+
+  attendee.ticketType = newTicketType;
+  
+}
+
+//Function to update the price of ticket
+function updateTicketPrice(attendee, newTicketPrice){
+
+  attendee.ticketPrice = newTicketPrice;
+
+}
+
+//Function to remove the event property from the object
+function removeEventProperty(attendee){
+
+  delete attendee.event;
+
+}
+
+//Function to add a property for whether the attendee checked in
+function addCheckedInProperty(attendee){
+
+  attendee.checkedIn = true;
+
+}
 
 //Needed for the tests to work. Don't modify
 module.exports = {
